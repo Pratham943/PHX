138a998d19a1237f9b4fbaa854f310a87fbad08e
Pratham943 <phoneixwasfound69@gmail.com>
2024-06-23 15:05:45 +0545

    i dunno

--- js/9-github.js
+++ js/9-github.js
@@ -1,1 +1,22 @@
+let user = {
+    name : "ram" ,
+    paidStatus : false ,
+    hasScholarship : false ,
+    gender : "female" ,
+}
+
+if(user.paidStatus){
+    console.log(`${user.name} can give exam `);
+ } else {
+   if(user.hasScholarship)  {
+    console.log(`${user.name} can give exam `);
+   }else{
+        if(user.gender == "male"){
+            console.log(`mr.${user.name} can give exam `);
+        }else{
+            console.log(`mrs.${user.name} can't give exam `);
+        }
+    }    
+}
+
 
